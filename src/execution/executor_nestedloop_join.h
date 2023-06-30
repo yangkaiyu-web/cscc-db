3606eb1388a30d52da60ce510286982c51e1cd69
yonchicy <1079279184@qq.com>
2023-06-30 22:07:21 +0800

    暂存没实现完的join

--- src/execution/executor_nestedloop_join.h
+++ src/execution/executor_nestedloop_join.h
@@ -9,10 +9,12 @@ MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 See the Mulan PSL v2 for more details. */
 
 #pragma once
+#include <memory>
 #include "execution_defs.h"
 #include "execution_manager.h"
 #include "executor_abstract.h"
 #include "index/ix.h"
+#include "record/rm_defs.h"
 #include "system/sm.h"
 
 class NestedLoopJoinExecutor : public AbstractExecutor {
@@ -23,7 +25,9 @@ class NestedLoopJoinExecutor : public AbstractExecutor {
     std::vector<ColMeta> cols_;                 // join后获得的记录的字段
 
     std::vector<Condition> fed_conds_;          // join条件
-    bool isend;
+    bool is_end_;
+    RmRecord rec_;
+  std::vector<std::unique_ptr<RmRecord>> left_record_save_;
 
    public:
     NestedLoopJoinExecutor(std::unique_ptr<AbstractExecutor> left, std::unique_ptr<AbstractExecutor> right, 
@@ -38,13 +42,24 @@ class NestedLoopJoinExecutor : public AbstractExecutor {
         }
 
         cols_.insert(cols_.end(), right_cols.begin(), right_cols.end());
-        isend = false;
+        is_end_ = false;
         fed_conds_ = std::move(conds);
 
     }
 
     void beginTuple() override {
+        for(left_->beginTuple();!left_->is_end();left_->nextTuple()){
+          left_record_save_.push_back(left_->Next());
+        }
+        right_->beginTuple();
+        if(!right_->is_end()){
+            auto right_rec =  right_->Next();
+            
 
+            
+        }
+        
+    
     }
 
     void nextTuple() override {
