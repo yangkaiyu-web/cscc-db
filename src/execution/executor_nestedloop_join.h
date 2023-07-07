04a5ffe3416fedfb53e84617621e51225654b42c
yonchicy <1079279184@qq.com>
2023-07-07 11:18:59 +0800

    修复 join 的时候，忘了保存 left_ 节点迭代状态的 bug ，目前存在 bug 如下
    出现了 disk_manager::read_page 的问题

--- src/execution/executor_nestedloop_join.h
+++ src/execution/executor_nestedloop_join.h
@@ -1,7 +1,7 @@
 /* Copyright (c) 2023 Renmin University of China
 RMDB is licensed under Mulan PSL v2.
-You can use this software according to the terms and conditions of the Mulan PSL v2.
-You may obtain a copy of Mulan PSL v2 at:
+You can use this software according to the terms and conditions of the Mulan PSL
+v2. You may obtain a copy of Mulan PSL v2 at:
         http://license.coscl.org.cn/MulanPSL2
 THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
 EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
@@ -10,6 +10,7 @@ See the Mulan PSL v2 for more details. */
 
 #pragma once
 #include <memory>
+
 #include "execution_defs.h"
 #include "execution_manager.h"
 #include "executor_abstract.h"
@@ -19,101 +20,108 @@ See the Mulan PSL v2 for more details. */
 
 class NestedLoopJoinExecutor : public AbstractExecutor {
    private:
-    std::unique_ptr<AbstractExecutor> left_;    // 左儿子节点（需要join的表）
-    std::unique_ptr<AbstractExecutor> right_;   // 右儿子节点（需要join的表）
-    ssize_t len_;                                // join后获得的每条记录的长度
-    std::vector<ColMeta> cols_;                 // join后获得的记录的字段
+    std::unique_ptr<AbstractExecutor> left_;  // 左儿子节点（需要join的表）
+    std::unique_ptr<AbstractExecutor> right_;  // 右儿子节点（需要join的表）
+    ssize_t len_;                // join后获得的每条记录的长度
+    std::vector<ColMeta> cols_;  // join后获得的记录的字段
 
-    std::vector<Condition> fed_conds_;          // join条件
+    std::vector<Condition> fed_conds_;  // join条件
     bool is_end_;
     RmRecord rec_;
-  std::vector<std::unique_ptr<RmRecord>> left_record_save_;
+    std::vector<std::unique_ptr<RmRecord>> left_record_save_;
+    bool left_cache_valid_;
+    size_t left_index_;
 
    public:
-    NestedLoopJoinExecutor(std::unique_ptr<AbstractExecutor> left, std::unique_ptr<AbstractExecutor> right, 
-                            std::vector<Condition> conds) {
+    NestedLoopJoinExecutor(std::unique_ptr<AbstractExecutor> left,
+                           std::unique_ptr<AbstractExecutor> right,
+                           std::vector<Condition> conds) {
         left_ = std::move(left);
         right_ = std::move(right);
         len_ = left_->tupleLen() + right_->tupleLen();
         cols_ = left_->cols();
         auto right_cols = right_->cols();
-        for (auto &col : right_cols) {
+        for (auto& col : right_cols) {
             col.offset += left_->tupleLen();
         }
 
         cols_.insert(cols_.end(), right_cols.begin(), right_cols.end());
         is_end_ = false;
         fed_conds_ = std::move(conds);
-
+        rec_ = RmRecord(len_);
+        left_index_ = 0;  // 标志当前已经消耗完第几个 left_record_save_
     }
 
     void beginTuple() override {
         // TODO:  交换? 比如：select * from t1,t2 on t2.id = t1.id;
-        for(left_->beginTuple();!left_->is_end();left_->nextTuple()){
-          left_record_save_.push_back(left_->Next());
+        for (left_->beginTuple(); !left_->is_end(); left_->nextTuple()) {
+            left_record_save_.push_back(left_->Next());
         }
-        if(!left_record_save_.empty()){
-            for(right_->beginTuple();!right_->is_end();right_->nextTuple()){
-
-                auto right_rec =  right_->Next();
-                for(auto& left_record:left_record_save_){
-
+        if (!left_record_save_.empty()) {
+            for (right_->beginTuple(); !right_->is_end(); right_->nextTuple()) {
+                auto right_rec = right_->Next();
+                for (left_index_ = 0; left_index_ < left_record_save_.size();
+                     left_index_++) {
+                    auto& left_record = left_record_save_[left_index_];
                     auto flag = true;
-                    for(auto& cond: fed_conds_){
-
-                        flag = flag && cond.test_join_record(left_->cols(),left_record,right_->cols(),right_rec);
+                    for (auto& cond : fed_conds_) {
+                        flag = flag &&
+                               cond.test_join_record(left_->cols(), left_record,
+                                                     right_->cols(), right_rec);
                     }
-                    if(flag){
-                        memcpy(rec_.data,left_record->data,left_->tupleLen());
-                        memcpy(rec_.data+left_->tupleLen(),right_rec->data,right_->tupleLen());
+                    if (flag) {
+                        memcpy(rec_.data, left_record->data, left_->tupleLen());
+                        memcpy(rec_.data + left_->tupleLen(), right_rec->data,
+                               right_->tupleLen());
                         return;
                     }
                 }
             }
         }
-
-
     }
 
     void nextTuple() override {
-        right_->nextTuple();
-        while(!right_->is_end()){
-                auto right_rec =  right_->Next();
-                for(auto& left_record:left_record_save_){
-
-                    auto flag = true;
-                    for(auto& cond: fed_conds_){
-
-                        flag = flag && cond.test_join_record(left_->cols(),left_record,right_->cols(),right_rec);
-                    }
-                    if(flag){
-                        memcpy(rec_.data,left_record->data,left_->tupleLen());
-                        memcpy(rec_.data+left_->tupleLen(),right_rec->data,right_->tupleLen());
-                        return;
-                    }
-                }
+        left_index_ ++;
+        if (left_index_ == left_record_save_.size()) {
             right_->nextTuple();
+            left_index_ = 0;
         }
+        while (!right_->is_end()) {
+            auto right_rec = right_->Next();
+            while (left_index_ < left_record_save_.size()) {
+                auto& left_record = left_record_save_[left_index_];
+                auto flag = true;
+                for (auto& cond : fed_conds_) {
+                    flag = flag &&
+                           cond.test_join_record(left_->cols(), left_record,
+                                                 right_->cols(), right_rec);
+                }
+                if (flag) {
+                    memcpy(rec_.data, left_record->data, left_->tupleLen());
+                    memcpy(rec_.data + left_->tupleLen(), right_rec->data,
+                           right_->tupleLen());
+                    return;
+                }
+                left_index_++;
+            }
+        }
+        right_->nextTuple();
     }
 
     std::unique_ptr<RmRecord> Next() override {
-        auto ptr = std::make_unique<RmRecord>(len_);
-        ptr->SetData(rec_.data);
+        auto ptr = std::make_unique<RmRecord>(rec_);
         return ptr;
     }
-    
-    bool is_end ()const  override {
+
+    bool is_end() const override {
         bool ret = right_->is_end() || left_record_save_.empty();
         return ret;
     }
 
-    size_t tupleLen() const override{ return len_; };
+    size_t tupleLen() const override { return len_; };
 
-    const std::vector<ColMeta> &cols() const override{
-        
-        return cols_;
-    };
+    const std::vector<ColMeta>& cols() const override { return cols_; };
 
-    std::string getType() override{ return "NestedLoopJoinExecutor"; };
-    Rid &rid() override { return _abstract_rid; }
+    std::string getType() override { return "NestedLoopJoinExecutor"; };
+    Rid& rid() override { return _abstract_rid; }
 };
