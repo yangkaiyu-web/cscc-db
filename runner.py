be1a22871bcb92735254730c5ed8a0cfa4a874c1
yonchicy <1079279184@qq.com>
2023-07-10 02:18:06 +0000

    测试脚本基本写完了

--- runner.py
+++ runner.py
@@ -1,31 +1,85 @@
 import argparse
 import os
 import subprocess
+import time
+import signal
 
 
-def remote_run():
-    ssh_str = "ssh nao"
-    mysql_str = "mysql -u root -p1234 < drop.sql"
+def bench_run():
 
-    subprocess.run([ssh_str,mysql_str])
+    print("running bench mark")
+    drop_db = "drop database testdb;"
+    create_db = "create database testdb;"
+    mysql_str = "mysql -u root -p1234"
+    p1 = subprocess.Popen(mysql_str.split(),stdin=subprocess.PIPE)
+    p1.communicate(drop_db.encode())
+    p1.wait()
+    p2 = subprocess.Popen(mysql_str.split(),stdin=subprocess.PIPE)
+    p2.communicate(create_db.encode())
+    p2.wait()
     
+    file = open("rmdb_client/build/a.sql",'r')
+    
+    p3= subprocess.Popen(mysql_str.split(),stdin=file,stdout=subprocess.PIPE)
+    output,errors = p3.communicate()
+    p3.wait()
+    result = output.decode().splitlines()
+    res_file  = open("rmdb_client/build/a.txt","w")
+    for line in result:
+        res_file.write('|')
+        for word in line.split():
+            res_file.write(' '+word+' |')
+        res_file.write('\n')
+    res_file.close()
+            
+
+def my_run():
+
+    print("running rmdb")
+    os.chdir('build')
+    if os.path.exists('testdb'):
+        os.rmdir("testdb")
+    svr = "./bin/rmdb testdb"
+    p1 = subprocess.Popen(svr.split(),preexec_fn=os.setsid)
+    time.sleep(1)
+    os.chdir("..")
+    client = "./rmdb_client/build/rmdb_client"
+    file = open("rmdb_client/build/a.sql",'r')
+    
+    p2= subprocess.Popen(client.split(),stdin=file,stdout=subprocess.PIPE)
+    output,errors = p2.communicate()
+    p2.wait()
+    p1.send_signal(signal.SIGINT)
     
 
 def build():
     # build svr
 
     print("building rmdb...")
+    if not os.path.exists("./build") :
+        os.makedirs("./build")
+
+    if not os.path.exists("./build/Makefile"):
+        os.chdir("./build")
+        subprocess.run(['cmake','..','-DCMAKE_EXPORT_COMPILE_COMMANDS=1'])
+        os.chdir("..")
+
+
     os.chdir("./build")
-    svr_build_result =  subprocess.run(['make','-j20'],capture_output=True,text=True)
-    print(svr_build_result.stderr)
-    print(svr_build_result.stdout)
+    subprocess.run(['make','-j20'])
     os.chdir("..")
+    
 
     print("building rmdb client")
+    if not os.path.exists("./rmdb_client/build"):
+        os.makedirs("./rmdb_client/build")
+    if not os.path.exists("./rmdb_client/build/Makefile"):
+        os.chdir("./rmdb_client/build")
+        subprocess.run(['cmake','..','-DCMAKE_EXPORT_COMPILE_COMMANDS=1'])
+        os.chdir("../..")
     os.chdir("./rmdb_client/build")
-    svr_build_result =  subprocess.run(['make','-j20'],capture_output=True,text=True)
-    print(svr_build_result.stderr)
-    print(svr_build_result.stdout)
+    subprocess.run(['make','-j20'])
+    
     os.chdir("../..")
 
 
@@ -44,7 +98,8 @@ def main():
     if args.build:
         build()
     if args.run:
-        remote_run()
+        bench_run()
+        my_run()
     return 0
 
 
