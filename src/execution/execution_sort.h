767f7874e67a2a8469e61b0eebdf7ead4006ab76
yonchicy <1079279184@qq.com>
2023-07-26 02:25:44 +0000

    修改 sort 里面 unique ptr double free 的 问题

--- src/execution/execution_sort.h
+++ src/execution/execution_sort.h
@@ -175,7 +175,7 @@ class SortExecutor : public AbstractExecutor {
                         // 读取对应块文件的下一行
 
                         streams[index].read(datas[index]->data, prev_->tupleLen());
-                        if (streams.back().good()) {
+                        if (streams.back().good() && !streams.back().eof()) {
                             continue;
                         }
 
@@ -234,13 +234,13 @@ class SortExecutor : public AbstractExecutor {
 
     // 如果是 desc， 那么大的先落盘，
     int find_element(std::vector<std::unique_ptr<RmRecord>>& datas) {
-        auto tmp = std::unique_ptr<RmRecord>(datas.front().get());
+        auto tmp = datas.begin();
         int index = 0;
         for (size_t i = 1; i < datas.size(); i++) {
-            auto rec = std::unique_ptr<RmRecord>(datas[i].get());
-            int ret = cmp(tmp, rec, cols_);
+            auto rec = datas.begin()+i;
+            int ret = cmp(*tmp, *rec, cols_);
             if (ret < 0) {
-                tmp = std::unique_ptr<RmRecord>(rec.get());
+                tmp = rec;
                 index = i;
             }
         }
