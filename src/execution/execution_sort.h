5dd0edff30b94b6f53b617297ba3dcc3f9945e62
yonchicy <1079279184@qq.com>
2023-07-26 16:59:55 +0800

    基本完成 sort

--- src/execution/execution_sort.h
+++ src/execution/execution_sort.h
@@ -9,10 +9,15 @@ MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 See the Mulan PSL v2 for more details. */
 
 #pragma once
+#include <fcntl.h>
+
+#include <chrono>
+#include <cstdio>
 #include <cstring>
 #include <ctime>
 #include <fstream>
 #include <memory>
+#include <ratio>
 #include <string>
 #include <utility>
 
@@ -27,6 +32,40 @@ See the Mulan PSL v2 for more details. */
 #include "system/sm.h"
 #include "system/sm_manager.h"
 
+struct TupleBufFile {
+    std::string file_name;
+    int fd;
+    size_t tuple_len;
+    size_t total_tuple_num;
+    size_t written_num;
+    size_t read_num;
+    TupleBufFile(std::string& file_name_, size_t tuple_len_) : file_name(file_name_), tuple_len(tuple_len_) {
+        total_tuple_num = written_num = read_num = 0;
+    }
+    TupleBufFile() : file_name(""), tuple_len(0) {
+        total_tuple_num = written_num = read_num = 0;
+    }
+    int open_write() {
+        fd = open(file_name.c_str(), O_CREAT | O_RDWR,0777);
+        return fd;
+    }
+    int open_read() {
+        fd = open(file_name.c_str(), O_RDWR,0777);
+        return fd;
+    }
+    size_t write(const char* buf, size_t tuple_num) {
+        size_t num = ::write(fd, buf, tuple_num * tuple_len);
+        written_num += tuple_num;
+        return num/tuple_len;
+    }
+    size_t read(char* buf, size_t tuple_num) {
+        size_t num = ::read(fd, buf, tuple_num * tuple_len);
+        read_num += tuple_num;
+        return num/tuple_len;
+    }
+    int close() { return ::close(fd); }
+    bool is_eof() { return written_num == read_num; }
+};
 class SortExecutor : public AbstractExecutor {
    private:
     // TODO: 直接申请内存还是从bufferpool里获取？
@@ -35,11 +74,11 @@ class SortExecutor : public AbstractExecutor {
 
     std::unique_ptr<AbstractExecutor> prev_;
     std::vector<std::pair<ColMeta, bool>> cols_;  // 框架中只支持一个键排序，需要自行修改数据结构支持多个键排序
-    int tuple_num;
+    size_t tuple_num_;
     int used_tuple_num;
     int limit_num_;
 
-    std::ifstream res_file_;
+    std::shared_ptr<TupleBufFile> res_file_;
     std::string res_file_name_;
     std::vector<size_t> used_tuple;
     RmRecord current_tuple_;
@@ -51,7 +90,7 @@ class SortExecutor : public AbstractExecutor {
             cols_.push_back(std::make_pair(prev_->get_col_offset(col.first), col.second));
         }
 
-        tuple_num = 0;
+        tuple_num_ = 0;
         used_tuple_num = 0;
         limit_num_ = limit_num;
         used_tuple.clear();
@@ -78,7 +117,7 @@ class SortExecutor : public AbstractExecutor {
         }
         return ret;
     }
-    void sort(char* buf, int tuple_number) {
+    void sort(char* buf, size_t tuple_number) {
         auto tuple_len = prev_->tupleLen();
         // TODO:低效，反复申请新内存，如何利用起来已经获取的 page
         for (int i = 0; i < tuple_number - 1; i++) {
@@ -99,23 +138,30 @@ class SortExecutor : public AbstractExecutor {
     }
     std::string getTime() {
         std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
-        std::chrono::duration<long long, std::milli> timestamp =
-            std::chrono::time_point_cast<std::chrono::milliseconds>(now).time_since_epoch();
+        std::chrono::duration<long long, std::nano> timestamp =
+            std::chrono::time_point_cast<std::chrono::nanoseconds>(now).time_since_epoch();
         return std::to_string(timestamp.count());
     }
-    void writeTmpData(char* read_buf, int tmp_file_count, std::vector<std::string>& tmp_file_names) {
-        std::string tmp_filename = "sort" + std::to_string(tmp_file_count) + ".tmp" + getTime();
-        std::ofstream tmpfile(tmp_filename, std::ios::binary);
-        tmpfile.write(read_buf, PAGE_SIZE);
-        tmpfile.close();
-        tmp_file_names.push_back(tmp_filename);
+    void writeTmpData(char* read_buf, std::vector<std::shared_ptr<TupleBufFile>>& tmp_files, size_t tuple_count) {
+        std::string tmp_file_name = "sort.tmp" + getTime();
+
+        auto  tmp_file = std::make_shared<TupleBufFile>(tmp_file_name, prev_->tupleLen());
+        if (tmp_file->open_write() < 0) {
+            throw UnixError();
+        }
+        if (tmp_file->write(read_buf, tuple_count) != tuple_count ) {
+            throw UnixError();
+        }
+        tmp_file->close();
+        tmp_files.push_back(tmp_file);
     }
     // TODO: 缓存一下排序结果，防止反复 begin
     void beginTuple() override {
-        tuple_num = 0;
+        tuple_num_ = 0;
         used_tuple_num = 0;
         std::vector<std::string> tmp_file_names;
-        int tmp_file_count = 0;
+        std::vector<std::shared_ptr<TupleBufFile>> tmp_files;
+
         auto tuple_num_per_page = PAGE_SIZE / prev_->tupleLen();
         size_t tuple_len = prev_->tupleLen();
         prev_->beginTuple();
@@ -128,107 +174,62 @@ class SortExecutor : public AbstractExecutor {
             tuple_num_on_page++;
             if (tuple_num_on_page == tuple_num_per_page) {
                 sort(read_buf, tuple_num_on_page);
-                writeTmpData(read_buf, tmp_file_count++, tmp_file_names);
-                tuple_num += tuple_num_on_page;
+                writeTmpData(read_buf, tmp_files, tuple_num_on_page);
+                tuple_num_ += tuple_num_on_page;
                 tuple_num_on_page = 0;
             }
             prev_->nextTuple();
         }
         if (tuple_num_on_page != 0) {
-            tuple_num += tuple_num_on_page;
+            tuple_num_ += tuple_num_on_page;
             sort(read_buf, tuple_num_on_page);
-            writeTmpData(read_buf, tmp_file_count++, tmp_file_names);
+            writeTmpData(read_buf, tmp_files, tuple_num_on_page);
             tuple_num_on_page = 0;
         }
 
         // merge
-        std::vector<std::string> res_file_names;
-        std::vector<std::ifstream> streams;
+
+
         std::vector<std::unique_ptr<RmRecord>> datas;
         // std::copy(tmp_file_names.begin(), tmp_file_names.end(), res_file_names.begin());
-        res_file_names = tmp_file_names;
-        while (res_file_names.size() > 1) {
-            tmp_file_names.clear();
+
+        while (tmp_files.size() > 1) {
+            std::vector<std::shared_ptr<TupleBufFile>> used_files;
             // std::copy(res_file_names.begin(), res_file_names.end(), tmp_file_names.begin());
-            tmp_file_names = res_file_names;
-            res_file_names.clear();
-            auto block_it = tmp_file_names.begin();
-            while (block_it != tmp_file_names.end()) {
-                std::ifstream inputFile(*block_it, std::ios::binary);
-                streams.push_back(std::move(inputFile));
+            auto block_it = tmp_files.begin();
+            while (block_it != tmp_files.end()) {
+                used_files.push_back(*block_it);
                 auto rec = std::make_unique<RmRecord>(prev_->tupleLen());
-                streams.back().read(rec->data, prev_->tupleLen());
-                if (streams.back().good()) {
+                if(used_files.back()->open_read()<0){
+                    throw UnixError();
+                }
+                if (used_files.back()->read(rec->data, 1) != 1) {
+                    throw UnixError();
+                }
+                if (used_files.back()->read_num <= used_files.back()->written_num) {
                     datas.push_back(std::move(rec));
+                } else {
+                    throw InternalError("error");
                 }
 
-                if (streams.size() == tuple_num_on_page) {
-                    std::string output_file_name = "sort.tmp" + getTime();
-                    std::ofstream output_file(output_file_name, std::ios::binary);
-                    res_file_names.push_back(output_file_name);
-
-                    while (!datas.empty()) {
-                        auto index = find_element(datas);
-
-                        output_file.write(datas[index]->data, prev_->tupleLen());
-
-                        // 读取对应块文件的下一行
-
-                        streams[index].read(datas[index]->data, prev_->tupleLen());
-                        if (streams.back().good() && !streams.back().eof()) {
-                            continue;
-                        }
-
-                        // 当前块文件已经读取完，关闭文件并从内存中移除
-                        streams[index].close();
-                        if (remove(block_it->c_str()) != 0) {
-                            throw UnixError();
-                        }
-                        datas.erase(datas.begin() + index);
-                        streams.erase(streams.begin() + index);
-                    }
-                    assert(datas.empty());
-                    assert(streams.empty());
-                    output_file.close();
+                if (used_files.size() == tuple_num_per_page) {
+                    mergeTmpFiles(datas, used_files, tmp_files);
                 }
                 block_it++;
             }
-            if (!streams.empty()) {
-                std::string output_file_name = "sort.tmp" + getTime();
-                std::ofstream output_file(output_file_name, std::ios::binary);
-                res_file_names.push_back(output_file_name);
-
-                while (!datas.empty()) {
-                    auto index = find_element(datas);
-
-                    output_file.write(datas[index]->data, prev_->tupleLen());
-
-                    // 读取对应块文件的下一行
-
-                    streams[index].read(datas[index]->data, prev_->tupleLen());
-                    if (streams.back().good()) {
-                        continue;
-                    }
-
-                    // 当前块文件已经读取完，关闭文件并从内存中移除
-                    streams[index].close();
-                    datas.erase(datas.begin() + index);
-                    streams.erase(streams.begin() + index);
-                }
-                assert(datas.empty());
-                assert(streams.empty());
-                output_file.close();
+            if (!used_files.empty()) {
+                mergeTmpFiles(datas, used_files, tmp_files);
             }
-            tmp_file_names.clear();
+
+
             // std::copy(tmp_file_names.begin(), tmp_file_names.end(), res_file_names.begin());
-            res_file_names = tmp_file_names;
         }
-        assert(res_file_names.size() == 1);
-        res_file_name_ = res_file_names[0];
-        res_file_ = std::ifstream(res_file_name_, std::ios::binary);
+        assert(tmp_files.size() == 1);
+        res_file_ = tmp_files[0];
+        res_file_->open_read();
         current_tuple_ = RmRecord(prev_->tupleLen());
 
-        res_file_.read(current_tuple_.data, prev_->tupleLen());
+        res_file_->read(current_tuple_.data, 1);
         used_tuple_num++;
     }
 
@@ -237,7 +238,7 @@ class SortExecutor : public AbstractExecutor {
         auto tmp = datas.begin();
         int index = 0;
         for (size_t i = 1; i < datas.size(); i++) {
-            auto rec = datas.begin()+i;
+            auto rec = datas.begin() + i;
             int ret = cmp(*tmp, *rec, cols_);
             if (ret < 0) {
                 tmp = rec;
@@ -246,14 +247,54 @@ class SortExecutor : public AbstractExecutor {
         }
         return index;
     }
+    void mergeTmpFiles(std::vector<std::unique_ptr<RmRecord>>& datas, std::vector<std::shared_ptr<TupleBufFile>>& streams,
+                       std::vector<std::shared_ptr<TupleBufFile>>& res_files) {
+        std::string output_file_name = "sort.tmp" + getTime();
+        auto  output_file = std::make_shared<TupleBufFile>(output_file_name, prev_->tupleLen());
+
+        output_file->open_write();
+        while (!datas.empty()) {
+            auto index = find_element(datas);
+
+            output_file->write(datas[index]->data, 1);
+
+            // 读取对应块文件的下一行
+
+            streams[index]->read(datas[index]->data, 1);
+            if (streams[index]->read_num <=streams[index]->written_num) {
+                continue;
+            }
+
+            // 当前块文件已经读取完，关闭文件并从内存中移除
+            streams[index]->close();
+            if(remove(streams[index]->file_name.c_str())<0){
+                throw UnixError();
+            }
+            for(auto bb=res_files.begin();bb!=res_files.end();bb++){
+                if((*bb)->file_name == (*(streams.begin()+index))->file_name){
+                    res_files.erase(bb);
+                    break;
+                }
+            }
+            datas.erase(datas.begin() + index);
+            streams.erase(streams.begin() + index);
+        }
+        assert(datas.empty());
+        assert(streams.empty());
+        output_file->close();
+        res_files.push_back(output_file);
+    }
 
     void nextTuple() override {
-        if (used_tuple_num < tuple_num || (limit_num_ > 0 && used_tuple_num < limit_num_)) {
-            res_file_.read(current_tuple_.data, prev_->tupleLen());
+        if (used_tuple_num < tuple_num_ || (limit_num_ > 0 && used_tuple_num < limit_num_)) {
+            res_file_->read(current_tuple_.data, 1);
             used_tuple_num++;
-        } else if (used_tuple_num == tuple_num || (limit_num_ > 0 && used_tuple_num == limit_num_)) {
-            res_file_.close();
-            if (remove(res_file_name_.c_str()) != 0) {
+        } else if (used_tuple_num == tuple_num_ || (limit_num_ > 0 && used_tuple_num == limit_num_)) {
+            if (used_tuple_num == tuple_num_) {
+                assert(res_file_->is_eof());
+            }
+            res_file_->close();
+            if (remove(res_file_->file_name.c_str()) != 0) {
                 throw UnixError();
             }
             used_tuple_num++;
@@ -274,7 +315,7 @@ class SortExecutor : public AbstractExecutor {
     bool is_end() const override {
         bool not_finish;
         if (limit_num_ < 0) {
-            not_finish = used_tuple_num <= tuple_num;
+            not_finish = used_tuple_num <= tuple_num_;
         } else if (limit_num_ >= 0) {
             not_finish = used_tuple_num <= limit_num_;
         } else {
