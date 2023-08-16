f9766979923096a93931b811462026095a6ff8c5
yonchicy <1079279184@qq.com>
2023-08-16 09:00:11 +0800

    删掉 重新创建db 的代码

--- src/rmdb.cpp
+++ src/rmdb.cpp
@@ -309,11 +309,7 @@ int main(int argc, char **argv) {
         if (!sm_manager->is_dir(db_name)) {
             // Database not found, create a new one
             sm_manager->create_db(db_name);
-        } else {
-            sm_manager->drop_db(db_name);
-            sm_manager->create_db(db_name);
-        }
-        // Open database
+        }         // Open database
         sm_manager->open_db(db_name);
 
         // recovery database
